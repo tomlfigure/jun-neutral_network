77b9897547803114ce58b5e9ae759716132e2c8d
idealtom <idealtom@163.com>
2023-12-28 13:50:07 +0800

    v2

--- test1.m
+++ test1.m
@@ -9,7 +9,7 @@ clear all
 A = [1 2 3; 4 5 6];
 
  
-%%
+%% 定义目标函数, 设置初始训练数据，创建BP神经网络
 
 %定义目标函数
 f = @(x, y) x .^ 2 - 2 * y;
@@ -17,12 +17,11 @@ f = @(x, y) x .^ 2 - 2 * y;
 %设置训练数据
 x_test = 2:2:20;
 y_test = 2:2:20;
-z_test = f(x_test, y_test);
+z_test_train = f(x_test, y_test);
 
 %数据归一化处理
 [x_test_train, psx] = mapminmax(x_test, 0, 1);
 [y_test_train, psy] = mapminmax(y_test, 0, 1);
-[z_test_train, psz] = mapminmax(z_test, 0, 1);
 
 %将x，y以列为组进行排列
 data_test_train = [x_test_train; y_test_train];
@@ -34,12 +33,25 @@ net = newff(data_test_train, z_test_train, 9);
 %%
 
 %设置训练参数
-% net.trainParam.goal = 1e-3;
-% net.trainParam.epochs = 100;
-% net.trainParam.lr = 0.01;
+net.trainParam.goal = 1e-3;
+net.trainParam.epochs = 100;
+net.trainParam.lr = 0.01;
+
+
+%设置新的训练数据
+
+x_new_train = 1:1:100;
+y_new_train = 1:1:100;
+
+[x_new_train_g, psx] = mapminmax(x_new_train, 0, 1);
+[y_new_train_g, psy] = mapminmax(y_new_train, 0, 1);
+
+z_new_train = f(x_new_train, y_new_train);
+
+data_new_train = [x_new_train_g; y_new_train_g];
 
 %用初始数据对网络进行100轮训练
-net = train(net,data_test_train, z_test_train);
+net = train(net, data_new_train, z_new_train);
 
 %%
 
@@ -56,16 +68,16 @@ y_test_verify_g = mapminmax('apply', y_test_verify, psy);
 
 
 %进行模型预测
-z_test_verify_g = sim(net, [x_test_verify_g; y_test_verify_g]);
+z_test_verify = sim(net, [x_test_verify_g; y_test_verify_g]);
 
-%将预测出的数据反归一化
-z_test_verify = mapminmax('reverse', z_test_verify_g, psz);
 
 %计算实际值与预测值的误差
 error = abs(z_test_verify_actual - z_test_verify);
 
+percentage = (error ./ z_test_verify_actual) .* 100;
+
 %以列的形式进行展示
-results = [z_test_verify_actual' , z_test_verify', error'];
+results = [z_test_verify_actual' , z_test_verify', error', percentage'];
 
 
 
