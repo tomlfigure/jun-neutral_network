d98f80e9ee85ac53a25635a015fed798f6bb3f99
review <idealtom@163.com>
2024-03-14 15:48:39 +0800

    modify1

--- refactor.m
+++ refactor.m
@@ -1,15 +1,17 @@
 
 %% 导入psx, psy and previous result for display
 
+load("data\keyData.mat");
+load("data\result_display.mat")
 %% 导入训练数据
-table_x = readtable('data_x.csv');
-table_y = readtable('data_y.csv');
+table_x = readtable('data\\data_x.csv');
+table_y = readtable('data\\data_y.csv');
 data_x = table2array(table_x)';
 [data_x_n, psx] = mapminmax(data_x, 0, 1);
 data_y = table2array(table_y)';
 [data_y_n, psy] = mapminmax(data_y, 0, 1);
 
-save('keyData.mat', 'psx', 'psy');
+save('data\\keyData.mat', 'psx', 'psy');
 
 %% 模型训练
 tic
@@ -57,18 +59,18 @@ toc
 
 
 %% 继续训练
-table_x = readtable('data_x.csv');
-table_y = readtable('data_y.csv');
+table_x = readtable('data\\data_x.csv');
+table_y = readtable('data\\data_y.csv');
 data_x = table2array(table_x)';
 data_y = table2array(table_y)';
 data_x_n = mapminmax('apply', data_x, psx);
 data_y_n = mapminmax('apply', data_y, psy);
-net = trainNetwork(data_x_n, data_y_n, net.layers, options);
+net = trainNetwork(data_x_n, data_y_n, net.Layers, options);
 clear table_x table_y data_x data_x_n data_y data_y_n
 
 %% 验证数据
-table_x_v = readtable('data_x_v.csv');
-table_y_v = readtable('data_y_v.csv');
+table_x_v = readtable('data\\data_x_v.csv');
+table_y_v = readtable('data\\data_y_v.csv');
 %验证数据
 data_x_v = table2array(table_x_v)';
 data_y_v = table2array(table_y_v)';
@@ -85,7 +87,7 @@ errors_percentage = errors ./ data_y_v' .* 100;
 disp(['               actualt                       ', 'predicted                   ', 'error                  ', 'error_percentage'])
 disp([data_y_v', data_y_p', errors, errors_percentage]);
 clear table_x_v table_y_v data_x_v data_x_v_n data_y_p_n
-save('result_display.mat', 'data_y_v', 'data_y_p', 'errors', 'errors_percentage');
+save('data\\result_display.mat', 'data_y_v', 'data_y_p', 'errors', 'errors_percentage');
 
 
 
